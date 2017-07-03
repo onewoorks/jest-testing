0773799df86a10869ab6abbea6f39103f133e356
iwangibrahim <irwanbibrahim@onewoorks.com>
2017-07-03 14:28:51 +0800

    .

---
+++ src/PositiveCounter.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PositiveCounter from './PositiveCounter'
+import renderer from 'react-test-renderer'
+
+it('should render 0', () => {
+    const tree = renderer.create(<PositiveCounter />).toJSON();
+    expect(tree).toMatchSnapshot();
+})
+
+it('should render 2', () => {
+    const component = renderer.create(<PositiveCounter />);
+    component.getInstance().increment();
+    component.getInstance().decrement();
+    expect(component.toJSON()).toMatchSnapshot();
+})
+
+it('shoud not go negative', () => {
+    const component = renderer.create(<PositiveCounter />);
+    component.getInstance().increment();
+    component.getInstance().decrement();
+    component.getInstance().decrement();
+    expect(component.toJSON).toMatchSnapshot();
+})
